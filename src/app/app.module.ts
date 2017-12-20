b233637419519a0a9ebfbedefa6a53c650e01e48
wenxingjie <wen_xingjie@163.com>
2017-12-20 17:49:29 +0800

    视图中利用 routerLink 实现路由的一个简单例子

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { HomeComponent } from './home/home.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const routes: Routes = [ // 这里存放着所有路线的集合，每条路线都用一个对象存放信息
+  // 对象最起码要有path和component这两个属性，path 表示路径，component表示满足路径时的组件
+  { path: '', component: HomeComponent},
+  { path: 'product', component: ProductComponent}  // 注意这里不用'/'开头，path不用斜杠开头，以后才能方便地使用相对路径和绝对路径
+];
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    [RouterModule.forRoot(routes)],
   ],
   providers: [],
   bootstrap: [AppComponent]
